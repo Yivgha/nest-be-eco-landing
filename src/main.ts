1f193ee7c84760835588387ddc188c052dcc8744
Yivgha <genieukne@gmail.com>
2024-05-03 23:12:38 +0300

    heroku configs

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3030'],
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3030',
+      'https://eco-landing-fe-942b0a9f4bb6.herokuapp.com/',
+      'https://eco-landing-8272c7ed5cfa.herokuapp.com/',
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
-  await app.listen(3030);
+  const PORT = process.env.PORT || 3000;
+  await app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 }
 bootstrap();
