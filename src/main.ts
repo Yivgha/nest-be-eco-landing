3611a4461a407d80721ae81874a13627b8ce8d3f
Yivgha <genieukne@gmail.com>
2024-05-08 14:49:10 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'http://localhost:3030',
-      'https://eco-landing-be-010e040111c7.herokuapp.com/',
-    ],
+    origin: '*',
+    //   [
+    //   'http://localhost:3000',
+    //   'http://localhost:3030',
+    //   'https://eco-landing-be-010e040111c7.herokuapp.com/'
+    // ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
